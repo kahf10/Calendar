3d299ba8f226d20306e98e2747d5cdd043adf025
Kahf Hussain <kahfnhussain@gmail.com>
2023-02-08 12:24:17 +0100

    Cleaned the repository and made the calendar work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+CalendarExample::Application.routes.draw do
+  resource :calendar, only: [:show], controller: :calendar
+  root to: "calendar#show"
 end
